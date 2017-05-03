abeb9b352c880f59222a1d34eb04b99b2ef10304
passager777 <liuzhibin_neu@163.com>
2017-05-03 15:31:54 +0800

    添加新建英雄和删除英雄功能

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,12 +20,30 @@ export class HeroesComponent implements OnInit {
     //在构造函数中初始化heroes，不推荐
     // this.heroes = this.heroService.getHeroes();
   }
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
   ngOnInit(){
     //初始化heroes
     this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
