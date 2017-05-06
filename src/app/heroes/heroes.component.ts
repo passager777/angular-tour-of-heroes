9e1e1b5186c1646fa018720989c91fd919bbd9db
passager777 <liuzhibin_neu@163.com>
2017-05-06 19:45:25 +0800

    提交--添加Angular动画功能，在状态与转场中实现修改了hero数据模型类相关配置

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition, keyframes} from '@angular/core';
 import {Hero} from "../model/hero";
 import { RouterModule, Routes, Router } from '@angular/router';
 import {HeroService} from '../service/hero.service';
+/**
 
+*/
 @Component({
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
   //template:`<>`
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  // animations: [
+  //   trigger('fadeIn', [
+  //     state('in', style({ display: 'none' })), // 默认元素不展开
+  //     transition('void => *', [ // 进场动画
+  //       animate(200, keyframes([
+  //         style({ height: '0', opacity: 0, offset: 0 }), // 元素高度0，元素隐藏(透明度为0)，动画帧在0%
+  //         style({ height: '*', opacity: 1, offset: 1 }) // 200ms后高度自适应展开，元素展开(透明度为1)，动画帧在100%
+  //       ]))
+  //     ]),
+  //     transition('* => void', [
+  //       animate(200, keyframes([
+  //         style({ height: '*', opacity: 1, offset: 0 }), // 与之对应，让元素从显示到隐藏一个过渡
+  //         style({ height: '0', opacity: 0, offset: 1 })
+  //       ]))
+  //     ]),
+  //   ])],
+  // animations: [//进场离场动画实现
+  //   trigger('flyInOut', [
+  //     state('in', style({transform: 'translateX(0)'})),
+  //     transition('void => *', [//进场动画
+  //       style({transform: 'translateX(-100%)'}),
+  //       animate(1000)
+  //     ]),
+  //     transition('* => void', [
+  //       animate(100, style({transform: 'translateX(100%)'}))
+  //     ])
+  //   ])
+  // ]
+  animations: [
+  /*
+    heroState动画实现(官网中'状态与转场'实现方式)：
+      手写修改了一下Hero model类，增加了hero的状态state，
+      在初始化in-memory-data.service.ts中给heroes中hero添加state状态
+      然后根据官网教程加入下面的animations动画属性，在页面html中引入有状态的
+      两种方式一种是不传递状态(@+animateName)，一种是传递状态的([@animateName]="hero.state")
+      此外在点击英雄时候，click事件触发函数加入修改此hero的state状态以通过状态改变来显示动画
+  */
+    trigger('heroState',[
+        state('inactive',style({
+          backgroundColor:"yellow",
+          transform: "scale(1)"
+        })),
+        state('active', style({
+          backgroundColor:"#cfd8dc",
+          transform: "scale(1.1)"
+        })),
+        transition('inactive => active', animate(100)),
+        transition('active => inactive', animate(100))
+      ])
+  ]
 })
 export class HeroesComponent implements OnInit {
 
@@ -47,6 +100,7 @@ export class HeroesComponent implements OnInit {
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    hero.state ="active";
   }
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
